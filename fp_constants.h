8108360e69c18c4557f852204111022f970348d2
Gustavo Banegas <gustavo@cryptme.in>
2025-04-20 09:57:58 +0200

    initial commit

---
+++ fp_constants.h
@@ -0,0 +1,22 @@
+#ifndef FP_CONSTANTS_H
+#define FP_CONSTANTS_H
+#include <stddef.h>
+#include <stdint.h>
+
+#define NWORDS_FIELD 9
+#define BITS 256
+#define LOG2P 8
+#define digit_t uint32_t
+#define sdigit_t int32_t
+#define RADIX 32
+#define LOG2RADIX 5
+#define NWORDS_ORDER 8
+
+typedef digit_t fp_t[NWORDS_FIELD];
+
+
+
+// Datatype for representing field elements
+
+#endif
+
